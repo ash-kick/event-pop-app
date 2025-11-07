93ef96b185908babeea5f00108285381245cb383
ash-kick <ashley.kicklighter@gmail.com>
2025-11-06 16:08:37 -0800

    adding content boxes to home page, created a test data file with a test user, updated max width for content boxes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,21 @@
+import { testUser1 } from "../../test-data/TestData";
+
 export default function Home() {
-     return <p>This is the home</p>;
+     return (
+          <div className="home-container">
+               <div className="home-content-box">
+                    <div className="home-welcome-message">
+                         <h1>Welcome back {testUser1.firstName}, let's see what's happening near you!</h1>
+                    </div>
+                    <div className="home-feeds-container">
+                         <div className="saved-event-feed">
+                              <h2>View Your Saved Events</h2>
+                         </div>
+                         <div className="upcoming-event-feed">
+                              <h2>Upcoming Events in Your Area</h2>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     );
 }
---
+++ src/test-data/TestData.jsx
@@ -0,0 +1,8 @@
+export const testUser1 = {
+     firstName: "Testing First",
+     lastName: "Testing Last",
+     userName: "atestuser",
+     email: "atestuser@testuser.com",
+     password: "xrwweriosl2",
+     locationId: "L92123",
+};
