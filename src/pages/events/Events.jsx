2119229b576e603319d17ed3e571663ae198c8b5
ash-kick <ashley.kicklighter@gmail.com>
2025-11-12 17:33:50 -0800

    adding button structure for events page move through event display

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+
 export default function Events() {
-     return <p>This is the events page</p>;
+     const [activeTab, setActiveTab] = useState("search");
+     console.log(activeTab);
+     return (
+          <div className="events-page-container">
+               <div className="event-tabs-container">
+                    <button
+                         className={`events-tab-button ${activeTab === "search" ? "active" : ""}`}
+                         onClick={() => {
+                              setActiveTab("search");
+                         }}>
+                         search
+                    </button>
+                    <button
+                         className={`events-tab-button ${activeTab === "saved" ? "active" : ""}`}
+                         onClick={() => {
+                              setActiveTab("saved");
+                         }}>
+                         saved
+                    </button>
+                    <button
+                         className={`events-tab-button ${activeTab === "upcoming" ? "active" : ""}`}
+                         onClick={() => {
+                              setActiveTab("upcoming");
+                         }}>
+                         upcoming
+                    </button>
+                    <div className="event-page-display">
+                         {activeTab === "search" ? "search events place holder" : ""}
+                         {activeTab === "saved" ? "saved events place holder" : ""}
+                         {activeTab === "upcoming" ? "upcoming events place holder" : ""}
+                    </div>
+               </div>
+          </div>
+     );
 }
