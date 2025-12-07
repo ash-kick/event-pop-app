fed387892863b0957cec4f9bee4e4dcec2e0fb54
ash-kick <ashley.kicklighter@gmail.com>
2025-12-07 12:22:36 -0800

    adding user model details and updating all model names to camel case

---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+     // TO BE USED IN MVP FOR APP
+     userName: {
+          type: String,
+          required: true,
+          unique: true,
+     },
+     password: {
+          type: String,
+          required: true,
+     },
+     email: {
+          type: String,
+          required: true,
+          unique: true,
+     },
+     eventPreferencesId: { type: String },
+     locationId: { type: String },
+
+     // FOR FUTURE STATE OF APP TO BE CREATED MODELS
+     alertsId: { type: String },
+     friendsId: { type: String },
+
+     createdAt: {
+          type: Date,
+          default: Date.now,
+     },
+     editedAt: {
+          type: Date,
+     },
+});
+
+module.exports = mongoose.model("User", userSchema);
