14775a39a9948868fc136cf8ac3e2a62d0f0ecd1
ash-kick <ashley.kicklighter@gmail.com>
2025-12-10 14:06:45 -0800

    un-ignoring test sync file so script is available as needed

---
+++ server/services/test-sync.js
@@ -0,0 +1,16 @@
+// require("dotenv").config();
+// const connectDB = require("../db");
+// const { fetchEventsForCity, cityNames } = require("./ticketmasterSyncService");
+
+// (async () => {
+//      await connectDB();
+//      console.log("Database connected, starting sync...");
+//      for (const cityName of cityNames) {
+//           await fetchEventsForCity(cityName);
+//           await new Promise((resolve) => setTimeout(resolve, 100));
+//      }
+//      console.log("Sync complete!");
+//      process.exit(0);
+// })();
+
+// this is my test script for loading mongo db initially, not to be used long term
