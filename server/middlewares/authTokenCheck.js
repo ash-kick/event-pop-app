77571df8887c1b312ba804d20dbf2f89af627fb5
ash-kick <ashley.kicklighter@gmail.com>
2025-12-11 10:35:18 -0800

    adding note about refresh tokens for future state

--- server/middlewares/authTokenCheck.js
+++ server/middlewares/authTokenCheck.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const JWT_SECRET_DEV = process.env.JWT_SECRET_DEV;
 
-// Check if token is present otherwise throw unathorized message
+// Check if token is present otherwise throw unathorized message, note only using access tokens for now, refresh token not yet implemented
 const authenticateToken = async (req, res, next) => {
      try {
           const token = req.headers["authorization"]?.split(" ")[1];
