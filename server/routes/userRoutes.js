8b8f60d0064adba171c6c7df483cdd806f1c0188
ash-kick <ashley.kicklighter@gmail.com>
2025-12-10 16:28:08 -0800

    adding userRoutes with registration route and adding app export from index file

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const userController = require("../controllers/userController");
+
+// ALL ROUTES RELATED TO USER DATA
+
+//POST create an account (user)
+router.post("/register", userController.registerUser);
