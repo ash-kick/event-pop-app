8763e2d16907b69b0123b67bb602bab49d72c783
ash-kick <ashley.kicklighter@gmail.com>
2025-12-10 16:34:04 -0800

    reorganizing index file and adding context

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db");
+const userRoutes = require("./routes/userRoutes");
 
+// Define port usring .env file
+const PORT = process.env.PORT || 3000;
+
+// Define app with express
 const app = express();
 
 // Middlewear
@@ -12,8 +17,10 @@ app.use(express.json());
 // Connect to db
 connectDB();
 
+// Use the following routes for the app
+app.use("/api/users", userRoutes);
+
 // Start server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
      console.log(`Server running on port ${PORT}`);
 });
