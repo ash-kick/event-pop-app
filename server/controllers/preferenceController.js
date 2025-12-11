a40570bab5a1817186bc41b2ec92be9be89f8ce7
ash-kick <ashley.kicklighter@gmail.com>
2025-12-11 10:50:55 -0800

    creating event preferences controller, adding comments for what functions need to be built

---
+++ server/controllers/preferenceController.js
@@ -0,0 +1,7 @@
+const EventPreference = require("../models/eventPreference");
+
+// create new preference
+
+// update preference
+
+// reset preference (will need a hardcoded default json file for the request)
