91393bb8c2203bcd52006884bb6f7d304ac96526
ash-kick <ashley.kicklighter@gmail.com>
2025-12-11 16:28:14 -0800

    adding comments for what routes need to be added to events

---
+++ server/routes/eventRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const eventController = require("../controllers/eventController");
+const authTokenCheck = require("../middlewares/authTokenCheck");
+
+//SAVED EVENT ROUTES
+
+// add saved event
+// delete saved event
+// display saved event
+
+//SEARCH EVENT ROUTES
+
+// get events based on search criteria
+
+// UPCOMING EVENT ROUTES
+
+// get upcoming events based on user location and current date
