8fd96d6f336dc995a74c3ffff8bc535fbeb982cb
ash-kick <ashley.kicklighter@gmail.com>
2025-12-18 17:18:19 -0800

    rearanging home and events pages

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -2,59 +2,10 @@ import { useState } from "react";
 import EventSearchDisplay from "./EventsSearchDisplay";
 
 export default function Events() {
-     const [activeTab, setActiveTab] = useState("search");
-     const [currentSearchBarValue, setCurrentSearchBarValue] = useState("");
-     const [submittedSearchTerm, setSubmittedSearchTerm] = useState("");
-
-     const handleSubmit = (e) => {
-          e.preventDefault();
-          setSubmittedSearchTerm(currentSearchBarValue);
-     };
-
      return (
           <div className="events-page-container">
-               <div className="event-tabs-container">
-                    <button
-                         className={`events-tab-button ${activeTab === "search" ? "active" : ""}`}
-                         onClick={() => {
-                              setActiveTab("search");
-                         }}>
-                         search
-                    </button>
-                    <button
-                         className={`events-tab-button ${activeTab === "saved" ? "active" : ""}`}
-                         onClick={() => {
-                              setActiveTab("saved");
-                         }}>
-                         saved
-                    </button>
-                    <button
-                         className={`events-tab-button ${activeTab === "upcoming" ? "active" : ""}`}
-                         onClick={() => {
-                              setActiveTab("upcoming");
-                         }}>
-                         upcoming
-                    </button>
-                    <div className="event-page-display">
-                         {activeTab === "search" ? (
-                              <div>
-                                   <form onSubmit={handleSubmit}>
-                                        <input
-                                             type="search"
-                                             placeholder="🔎 Search Events"
-                                             value={currentSearchBarValue}
-                                             onChange={(event) => setCurrentSearchBarValue(event.target.value)}
-                                        />
-                                        <button type="submit">Search</button>
-                                   </form>
-                                   <EventSearchDisplay submittedSearchTerm={submittedSearchTerm} />
-                              </div>
-                         ) : (
-                              ""
-                         )}
-                         {activeTab === "saved" ? "saved events place holder" : ""}
-                         {activeTab === "upcoming" ? "upcoming events place holder" : ""}
-                    </div>
+               <div className="saved-event-feed">
+                    <h2>View Your Saved Events</h2>
                </div>
           </div>
      );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,15 @@
-// need to add back in test data once in db
+import EventSearchDisplay from "../events/EventsSearchDisplay";
+import { useState } from "react";
 
 export default function Home() {
      const userName = localStorage.getItem("userName");
+     const [currentSearchBarValue, setCurrentSearchBarValue] = useState("");
+     const [submittedSearchTerm, setSubmittedSearchTerm] = useState("");
+
+     const handleSubmit = (e) => {
+          e.preventDefault();
+          setSubmittedSearchTerm(currentSearchBarValue);
+     };
      return (
           <div className="home-container">
                <div className="home-content-box">
@@ -9,8 +17,19 @@ export default function Home() {
                          <h1>Welcome back {userName}!</h1>
                     </div>
                     <div className="home-feeds-container">
-                         <div className="saved-event-feed">
-                              <h2>View Your Saved Events</h2>
+                         <div className="event-page-display">
+                              <div>
+                                   <form onSubmit={handleSubmit}>
+                                        <input
+                                             type="search"
+                                             placeholder="🔎 Search Events"
+                                             value={currentSearchBarValue}
+                                             onChange={(event) => setCurrentSearchBarValue(event.target.value)}
+                                        />
+                                        <button type="submit">Search</button>
+                                   </form>
+                                   <EventSearchDisplay submittedSearchTerm={submittedSearchTerm} />
+                              </div>
                          </div>
                          <div className="upcoming-event-feed">
                               <h2>Upcoming Events in Your Area</h2>
