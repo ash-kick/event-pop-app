43970398da7429c1c8a7de6c83118cacecbd2719
ash-kick <ashley.kicklighter@gmail.com>
2026-01-05 17:25:42 -0800

    adding handling for expired jwts and installing jwt decode, updating packages

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,5 @@
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 export default function ProtectedRoute({ children }) {
      const token = localStorage.getItem("token");
@@ -8,6 +9,31 @@ export default function ProtectedRoute({ children }) {
                <Navigate
                     to="/login"
                     // adding replace to prevent redirect loops
+                    replace
+               />
+          );
+     }
+     try {
+          const decodedToken = jwtDecode(token);
+          if (decodedToken.exp < Date.now() / 1000) {
+               localStorage.removeItem("token");
+               localStorage.removeItem("userName");
+               localStorage.removeItem("userCity");
+               return (
+                    <Navigate
+                         to="/login"
+                         // adding replace to prevent redirect loops
+                         replace
+                    />
+               );
+          }
+     } catch (err) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("userName");
+          localStorage.removeItem("userCity");
+          return (
+               <Navigate
+                    to="/login"
                     replace
                />
           );
