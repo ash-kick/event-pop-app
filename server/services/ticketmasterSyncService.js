85eb568eb8b63f7cc4532c3710fed1686ff6d6ac
ash-kick <ashley.kicklighter@gmail.com>
2026-01-06 12:41:29 -0800

    adding page limit and size checks to ensure we don't hit ticket master API limit

--- server/services/ticketmasterSyncService.js
+++ server/services/ticketmasterSyncService.js
@@ -6,6 +6,9 @@ const { CITY_TIMEZONES } = require("../constants/supportedCities");
 
 const TICKETMASTER_EVENTS_BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json?";
 const TICKETMASTER_API_KEY = process.env.TICKETMASTER_API_KEY;
+const MAX_ITEMS_LIMIT = 1000;
+const PAGE_SIZE = 20;
+const MAX_PAGE = Math.floor((MAX_ITEMS_LIMIT - 1) / PAGE_SIZE);
 
 const today = new Date();
 
@@ -43,93 +46,132 @@ const fetchEventsForCity = async (cityName) => {
                // create page related variables for looping through api response pages
                let pageNumber = 0;
                let hasMorePagesLeft = true;
+               let hitItemLimit = false;
 
                // pagination loop
-               while (hasMorePagesLeft) {
+               while (hasMorePagesLeft && !hitItemLimit) {
+                    // check if next page would go over ticketmaster item limit
+                    const currentItemCount = PAGE_SIZE * pageNumber;
+                    if (currentItemCount >= MAX_ITEMS_LIMIT) {
+                         console.log(
+                              `Reached ticketmaster API item limit ${MAX_ITEMS_LIMIT} for ${cityName} with date range ${dateRange.start} to ${dateRange.end}`
+                         );
+                         hitItemLimit = true;
+                         break;
+                    }
+                    // check if next page would go over ticket master page limit
+                    if (pageNumber > MAX_PAGE) {
+                         console.log(
+                              `Reached ticketmaster API page limit ${MAX_PAGE} for ${cityName} with date range ${dateRange.start} to ${dateRange.end}`
+                         );
+                         hitItemLimit = true;
+                         break;
+                    }
+
                     // ticket master api call full url inclues start date time
-                    let URL = `${TICKETMASTER_EVENTS_BASE_URL}apikey=${TICKETMASTER_API_KEY}&startDateTime=${dateRange.start}&endDateTime=${dateRange.end}&city=${cityName}&page=${pageNumber}&size=20`;
-                    const response = await axios.get(URL);
-                    console.log("Pagination info:", {
-                         page: response.data.page.number,
-                         size: response.data.page.size,
-                         totalElements: response.data.page.totalElements,
-                         totalPages: response.data.page.totalPages,
-                         hasNext: !!response.data._links?.next,
-                    });
-                    console.log("Events in this page:", cityName, response.data._embedded.events.length);
-
-                    // add these events to the data base
-                    const ticketMasterEvents = response.data._embedded.events;
-
-                    // add all event ids to the event id array
-                    allEventIds.push(...ticketMasterEvents.map((event) => event.id));
-
-                    // using bulk write for upserts and deletes
-                    // upsert logic
-                    for (const event of ticketMasterEvents) {
-                         // check for valid event date if there is none skip the event
-                         let eventDate = null;
-                         if (event.dates.start.localDate && event.dates.start.localTime) {
-                              eventDate = new Date(`${event.dates.start.localDate}T${event.dates.start.localTime}`);
-                         } else if (event.dates.start.dateTime) {
-                              eventDate = new Date(event.dates.start.dateTime);
-                         }
-                         // if the date is invalid skip to next event; note that isNaN(eventDate) is meant to check if a bad date was passed
-                         if (!eventDate || isNaN(eventDate)) {
-                              console.log(`Skipping event ${event.id} - no valid date found`);
-                              continue;
-                         }
-
-                         bulkOperations.push({
-                              updateOne: {
-                                   filter: { ticketMasterEventId: event.id },
-                                   update: {
-                                        $set: {
-                                             ticketMasterEventId: event.id,
-                                             eventName: event.name,
-                                             ticketMasterUrl: event.url,
-                                             eventImageUrl:
-                                                  event.images.find((image) => image.height > 600 && image.height < 800)?.url || event.images[0]?.url,
-                                             eventTypeId: event.classifications?.[0]?.segment?.id,
-                                             eventTypeName: event.classifications?.[0]?.segment?.name,
-                                             venueId: event._embedded.venues[0].id,
-                                             venueName: event._embedded.venues[0].name,
-                                             venueAddress: `${event._embedded.venues[0].address?.line1 || ""}${
-                                                  event._embedded.venues[0].address?.line2 ? ", " + event._embedded.venues[0].address.line2 : ""
-                                             }, ${event._embedded.venues[0].city?.name}, ${event._embedded.venues[0].state?.stateCode}, ${
-                                                  event._embedded.venues[0].country?.countryCode
-                                             }`,
-                                             startDateTimeUTC: event.dates.start.dateTime,
-                                             startDateTime: eventDate,
-                                             eventStatusCode: event.dates.status.code,
-                                             cityName: event._embedded.venues[0].city.name,
-                                             stateCode: event._embedded.venues[0].state.stateCode,
-                                             countryCode: event._embedded.venues[0].country.countryCode,
-                                             genreId: event.classifications?.[0]?.genre?.id,
-                                             genreName: event.classifications?.[0]?.genre?.name,
-                                             subGenreId: event.classifications?.[0]?.subGenre?.id,
-                                             subGenreName: event.classifications?.[0]?.subGenre?.name,
-                                             editedAt: today,
-                                        },
-                                        $setOnInsert: {
-                                             createdAt: today,
-                                        },
-                                   },
-                                   upsert: true,
-                              },
+                    try {
+                         let URL = `${TICKETMASTER_EVENTS_BASE_URL}apikey=${TICKETMASTER_API_KEY}&startDateTime=${dateRange.start}&endDateTime=${dateRange.end}&city=${cityName}&page=${pageNumber}&size=${PAGE_SIZE}`;
+                         const response = await axios.get(URL);
+                         console.log("Pagination info:", {
+                              page: response.data.page.number,
+                              size: response.data.page.size,
+                              totalElements: response.data.page.totalElements,
+                              totalPages: response.data.page.totalPages,
+                              hasNext: !!response.data._links?.next,
                          });
-                    }
+                         console.log("Events in this page:", cityName, response.data._embedded.events.length);
 
-                    // after bulk operations are added go ahead and set hasMorePagesLeft to be true or false
-                    if (response.data._links?.next && pageNumber < response.data.page.totalPages - 1) {
-                         hasMorePagesLeft = true;
-                         pageNumber++;
-                    } else {
-                         hasMorePagesLeft = false;
-                    }
+                         // add these events to the data base
+                         const ticketMasterEvents = response.data._embedded.events;
 
+                         // add all event ids to the event id array
+                         allEventIds.push(...ticketMasterEvents.map((event) => event.id));
+
+                         // using bulk write for upserts and deletes
+                         // upsert logic
+                         for (const event of ticketMasterEvents) {
+                              // check for valid event date if there is none skip the event
+                              let eventDate = null;
+                              if (event.dates.start.localDate && event.dates.start.localTime) {
+                                   eventDate = new Date(`${event.dates.start.localDate}T${event.dates.start.localTime}`);
+                              } else if (event.dates.start.dateTime) {
+                                   eventDate = new Date(event.dates.start.dateTime);
+                              }
+                              // if the date is invalid skip to next event; note that isNaN(eventDate) is meant to check if a bad date was passed
+                              if (!eventDate || isNaN(eventDate)) {
+                                   console.log(`Skipping event ${event.id} - no valid date found`);
+                                   continue;
+                              }
+
+                              bulkOperations.push({
+                                   updateOne: {
+                                        filter: { ticketMasterEventId: event.id },
+                                        update: {
+                                             $set: {
+                                                  ticketMasterEventId: event.id,
+                                                  eventName: event.name,
+                                                  ticketMasterUrl: event.url,
+                                                  eventImageUrl:
+                                                       event.images.find((image) => image.height > 600 && image.height < 800)?.url ||
+                                                       event.images[0]?.url,
+                                                  eventTypeId: event.classifications?.[0]?.segment?.id,
+                                                  eventTypeName: event.classifications?.[0]?.segment?.name,
+                                                  venueId: event._embedded.venues[0].id,
+                                                  venueName: event._embedded.venues[0].name,
+                                                  venueAddress: `${event._embedded.venues[0].address?.line1 || ""}${
+                                                       event._embedded.venues[0].address?.line2 ? ", " + event._embedded.venues[0].address.line2 : ""
+                                                  }, ${event._embedded.venues[0].city?.name}, ${event._embedded.venues[0].state?.stateCode}, ${
+                                                       event._embedded.venues[0].country?.countryCode
+                                                  }`,
+                                                  startDateTimeUTC: event.dates.start.dateTime,
+                                                  startDateTime: eventDate,
+                                                  eventStatusCode: event.dates.status.code,
+                                                  cityName: event._embedded.venues[0].city.name,
+                                                  stateCode: event._embedded.venues[0].state.stateCode,
+                                                  countryCode: event._embedded.venues[0].country.countryCode,
+                                                  genreId: event.classifications?.[0]?.genre?.id,
+                                                  genreName: event.classifications?.[0]?.genre?.name,
+                                                  subGenreId: event.classifications?.[0]?.subGenre?.id,
+                                                  subGenreName: event.classifications?.[0]?.subGenre?.name,
+                                                  editedAt: today,
+                                             },
+                                             $setOnInsert: {
+                                                  createdAt: today,
+                                             },
+                                        },
+                                        upsert: true,
+                                   },
+                              });
+                         }
+
+                         // after bulk operations are added go ahead and set hasMorePagesLeft to be true or false
+                         const nextPageItemCount = PAGE_SIZE * (pageNumber + 1);
+                         if (!response.data._links?.next || pageNumber >= response.data.page.totalPages - 1 || nextPageItemCount >= MAX_ITEMS_LIMIT) {
+                              hasMorePagesLeft = false;
+                              if (nextPageItemCount >= MAX_ITEMS_LIMIT) {
+                                   console.log(`Next page would exceed item limit. Stopping at page ${pageNumber} for ${cityName}`);
+                              }
+                         } else {
+                              pageNumber++;
+                         }
+                    } catch (apiError) {
+                         if (apiError.response?.status === 400 && apiError.response?.data?.fault?.faultstring?.includes("page")) {
+                              console.log(`API error: Page ${pageNumber} exceeds limit for ${cityName}.`);
+                              hitItemLimit = true;
+                              break;
+                         } else {
+                              //rethrow other errors to be caught by outer try/catch block
+                              throw apiError;
+                         }
+                    }
                     // add a little pause between requests
                     await new Promise((resolve) => setTimeout(resolve, 200));
+               }
+               // add warning if a limit has been hit and it's impacted the data pull
+               if (hitItemLimit) {
+                    console.log(
+                         `Warning: Some events may have been skipped for ${cityName} in date range ${dateRange.start} to ${dateRange.end} due to API limits.`
+                    );
                }
           }
 
