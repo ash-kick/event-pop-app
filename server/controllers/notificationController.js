16af21c9d2e609e6c0a8ee9c90fe2928e777f018
ash-kick <ashley.kicklighter@gmail.com>
2026-01-07 11:23:11 -0800

    creating notification controller, adding functionality for getting notifications for a user for display, marking notifications as read.

---
+++ server/controllers/notificationController.js
@@ -0,0 +1,53 @@
+const Notification = require("../models/notifications");
+
+// GET NOTIFICATIONS FOR A PARTICULAR USER
+exports.getNotifications = async (req, res, next) => {
+     try {
+          // get notifications for user and populate event info
+          const userNotifications = await Notification.find({ userId: req.user.id }).populate("eventId").sort({ createdAt: -1 });
+          // if there are no notifications return empty notifications and message stating this
+          if (userNotifications.length === 0) {
+               return res.status(200).json({
+                    notifications: [],
+                    message: "No notifications found",
+               });
+          }
+          // if there are notificaitions return them
+          return res.status(200).json({
+               notifications: userNotifications,
+          });
+     } catch (err) {
+          console.log("Could not retrieve notifications.");
+          next(err);
+     }
+};
+
+// MARK A SINGLE NOTIFICATION AS READ
+exports.markOneNotificationRead = async (req, res, next) => {
+     try {
+          const targetNotification = await Notification.findOneAndUpdate(
+               { _id: req.params.id, userId: req.user.id },
+               { read: true, updatedAt: new Date() },
+               { new: true }
+          );
+          if (!targetNotification) {
+               return res.status(404).json({ message: "Notification not found" });
+          }
+
+          res.status(200).json({ message: "Notification marked as read" }, targetNotification);
+     } catch (err) {
+          next(err);
+     }
+};
+// MARK ALL NOTIFICATIONS AS READ
+exports.markAllNotificationsRead = async (req, res, next) => {
+     try {
+          const allNotifications = await Notification.updateMany({ userId: req.user.id, read: false }, { read: true, updatedAt: new Date() });
+          if (!allNotifications) {
+               return res.status(404).json({ message: "Notifications not found" });
+          }
+          res.status(200).json({ message: "All notifications marked as read" });
+     } catch (err) {
+          next(err);
+     }
+};
