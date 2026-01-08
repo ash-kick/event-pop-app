5b959a30278cbe09a4108669b50b93604219c582
ash-kick <ashley.kicklighter@gmail.com>
2026-01-08 14:39:51 -0800

    creating saved event context

---
+++ src/contexts/SavedEventsContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const SavedEventContext = createContext();
+
+export function SavedEventProvider({ children }) {
+     const [savedEvents, setSavedEvents] = useState([]);
+     const [loading, setLoading] = useState(true);
+     const [error, setError] = useState(null);
+
+     const token = localStorage.getItem("token");
+     // fetch list of saved events
+     const fetchSavedEvents = async () => {
+          try {
+               const response = await axios.get("/api/events/saved-event", {
+                    headers: {
+                         Authorization: `Bearer ${token}`,
+                    },
+               });
+               setSavedEvents(response.data.savedEvents);
+               setError(null);
+               setLoading(false);
+          } catch (err) {
+               console.log("Error fetching saved events.");
+               setError(err.message);
+               setLoading(false);
+          }
+     };
+
+     useEffect(() => {
+          const token = localStorage.getItem("token");
+          if (token) {
+               fetchSavedEvents();
+          } else {
+               setSavedEvents([]);
+               setLoading(false);
+          }
+     }, []);
+
+     return (
+          <SavedEventContext.Provider
+               value={{
+                    savedEvents,
+                    loading,
+                    error,
+               }}>
+               {children}
+          </SavedEventContext.Provider>
+     );
+}
