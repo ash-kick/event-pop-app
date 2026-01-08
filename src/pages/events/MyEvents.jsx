eb26b1fc765fa7f9fa8f6506bffb55ee2347693e
ash-kick <ashley.kicklighter@gmail.com>
2026-01-08 15:36:27 -0800

    added display functionality for saved events

--- src/pages/events/MyEvents.jsx
+++ src/pages/events/MyEvents.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
-import EventSearchDisplay from "../../components/EventsSearchDisplay";
+import { useContext } from "react";
+import { SavedEventContext } from "../../contexts/SavedEventsContext";
 
 export default function MyEvents() {
+     const { savedEvents } = useContext(SavedEventContext);
      return (
           <div className="events-page-container">
                <div className="saved-event-feed">
                     <h2>View Your Saved Events</h2>
+                    <ul>
+                         {savedEvents?.map((event) => (
+                              <li>
+                                   <p>Event: {event.eventName}</p>
+                                   <p>Date: {event.startDateTime}</p>
+                                   <p>Venue: {event.venueName}</p>
+                              </li>
+                         ))}
+                    </ul>
                </div>
           </div>
      );
