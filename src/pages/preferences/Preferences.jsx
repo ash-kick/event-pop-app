66f6a747f0e460b4f2e289813237ec464d5dfe84
ash-kick <ashley.kicklighter@gmail.com>
2026-01-09 10:47:53 -0800

    refactoring alerts on logic for preferences page

--- src/pages/preferences/Preferences.jsx
+++ src/pages/preferences/Preferences.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 export default function Preferences() {
      const [preferences, setPreferences] = useState(null);
+     const [alertsOn, setAlertsOn] = useState(null);
      const token = localStorage.getItem("token");
 
      useEffect(() => {
@@ -14,6 +15,7 @@ export default function Preferences() {
                          },
                     });
                     setPreferences(response.data);
+                    setAlertsOn(response.data?.alertsOn);
                     console.log("Retrieved preferences!");
                } catch (err) {
                     console.log(err.message);
@@ -21,34 +23,24 @@ export default function Preferences() {
           };
           getPreferences();
      }, []);
+
+     // LEFT OFF HERE NEED TO FINISH FORM
      return (
           <div className="preferences-container">
                <h2>Preferences</h2>
                <form>
                     <label htmlFor="alerts-on">Alerts On</label>
-                    {preferences?.alertsOn ? (
-                         <select
-                              name="alerts-on"
-                              id="alerts-on">
-                              <option
-                                   value="true"
-                                   selected>
-                                   True
-                              </option>
-                              <option value="false">False</option>
-                         </select>
-                    ) : (
-                         <select
-                              name="alerts-on"
-                              id="alerts-on">
-                              <option value="true">True</option>
-                              <option
-                                   value="false"
-                                   selected>
-                                   False
-                              </option>
-                         </select>
-                    )}
+                    <select
+                         name="alerts-on"
+                         id="alerts-on"
+                         value={alertsOn === null ? "" : alertsOn.toString()}
+                         onChange={(e) => {
+                              // if the target value is true set alerts on to true (otherwise it will set alerts on to false)
+                              setAlertsOn(e.target.value === "true");
+                         }}>
+                         <option value="true">True</option>
+                         <option value="false">False</option>
+                    </select>
                     <label>Location</label>
                </form>
           </div>
