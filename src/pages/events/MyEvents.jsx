0e366a191009048b3229dc47d7b680ca3bfe71db
ash-kick <ashley.kicklighter@gmail.com>
2026-01-08 16:00:27 -0800

    added front end functionality for removing saved events

--- src/pages/events/MyEvents.jsx
+++ src/pages/events/MyEvents.jsx
@@ -1,18 +1,38 @@
 import { useContext } from "react";
 import { SavedEventContext } from "../../contexts/SavedEventsContext";
+import axios from "axios";
 
 export default function MyEvents() {
-     const { savedEvents } = useContext(SavedEventContext);
+     const { savedEvents, fetchSavedEvents } = useContext(SavedEventContext);
+     const token = localStorage.getItem("token");
+     async function handleClick(savedEvent) {
+          try {
+               const response = await axios.delete("/api/events/saved-event", {
+                    data: {
+                         eventId: savedEvent._id,
+                    },
+                    headers: {
+                         Authorization: `Bearer ${token}`,
+                    },
+               });
+               console.log(response.data.message);
+               await fetchSavedEvents();
+          } catch (err) {
+               console.log(err.message);
+          }
+     }
+
      return (
           <div className="events-page-container">
                <div className="saved-event-feed">
                     <h2>View Your Saved Events</h2>
                     <ul>
-                         {savedEvents?.map((event) => (
-                              <li>
-                                   <p>Event: {event.eventName}</p>
-                                   <p>Date: {event.startDateTime}</p>
-                                   <p>Venue: {event.venueName}</p>
+                         {savedEvents?.map((savedEvent) => (
+                              <li key={savedEvent._id}>
+                                   <p>Event: {savedEvent.eventName}</p>
+                                   <p>Date: {savedEvent.startDateTime}</p>
+                                   <p>Venue: {savedEvent.venueName}</p>
+                                   <button onClick={() => handleClick(savedEvent)}>Remove</button>
                               </li>
                          ))}
                     </ul>
