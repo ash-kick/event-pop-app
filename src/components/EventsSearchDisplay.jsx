fa42351dd94dad1dc71a1128444b0ce066f6f224
ash-kick <ashley.kicklighter@gmail.com>
2026-01-15 10:46:09 -0800

    adding classNames for styling serach event display:

--- src/components/EventsSearchDisplay.jsx
+++ src/components/EventsSearchDisplay.jsx
@@ -11,39 +11,62 @@ export default function EventSearchDisplay({ currentSearchResponse, searchError,
      }
      if (currentSearchResponse?.foundEvents) {
           return (
-               <div>
-                    <ul>
+               <div className="search-events-display">
+                    <ul className="search-events-list">
                          {currentSearchResponse.foundEvents.map((foundEvent) => (
                               <li key={foundEvent._id}>
-                                   <div>
-                                        <img src={foundEvent.eventImageUrl} />
+                                   <div className="display-event-image-container">
+                                        <img
+                                             className="diplay-event-image"
+                                             alt="image advertising event"
+                                             src={foundEvent.eventImageUrl}
+                                        />
                                    </div>
-                                   <div>{foundEvent.eventName}</div>
+                                   <div className="display-event-name">{foundEvent.eventName}</div>
                                    {foundEvent.eventTypeName && foundEvent.eventTypeName !== "Undefined" ? (
-                                        <div>
+                                        <div className="diplay-event-genre-sub-genre">
                                              {foundEvent.eventTypeName} {foundEvent.genreName ? `• ${foundEvent.genreName}` : null}{" "}
                                              {foundEvent.subGenreName ? `• ${foundEvent.subGenreName}` : null}
                                         </div>
                                    ) : null}
-                                   <div>
+                                   <div className="diplay-event-city-venue">
                                         {foundEvent.cityName} • {foundEvent.venueName}
                                    </div>
-                                   <div>{dayjs(foundEvent.startDateTime).format("ddd, MMM D • h:mm A")}</div>
+                                   <div className="diplay-event-date-time">{dayjs(foundEvent.startDateTime).format("ddd, MMM D • h:mm A")}</div>
                                    {foundEvent.ticketMasterUrl ? (
                                         <a
                                              href={foundEvent.ticketMasterUrl}
-                                             target="_blank">
+                                             target="_blank"
+                                             className="diplay-event-tickets-link">
                                              Get tickets!{" "}
                                         </a>
                                    ) : (
-                                        <div>Tickets currently unavailable ... event {foundEvent.eventStatusCode}</div>
+                                        <div className="diplay-event-tickets-unavailable">
+                                             Tickets currently unavailable ... event {foundEvent.eventStatusCode}
+                                        </div>
                                    )}
                                    <SaveEventButton eventId={foundEvent._id} />
                               </li>
                          ))}
                     </ul>
-                    {currentPage > 1 ? <button onClick={handlePreviousPage}>Previous</button> : <></>}
-                    {currentSearchResponse.hasMore ? <button onClick={handleNextPage}>Next</button> : <div>End of search results ...</div>}
+                    {currentPage > 1 ? (
+                         <button
+                              onClick={handlePreviousPage}
+                              className="previous-button">
+                              Previous
+                         </button>
+                    ) : (
+                         <></>
+                    )}
+                    {currentSearchResponse.hasMore ? (
+                         <button
+                              onClick={handleNextPage}
+                              className="next-button">
+                              Next
+                         </button>
+                    ) : (
+                         <div className="end-results-message">End of search results ...</div>
+                    )}
                </div>
           );
      }
