5c6f8e9de371cd4562294f0a9af2f447f69d8ea5
ash-kick <ashley.kicklighter@gmail.com>
2026-01-30 15:29:36 -0800

    adding checking for at least one option for each legend

--- tests/preferences.spec.js
+++ tests/preferences.spec.js
@@ -1,7 +1,7 @@
-import {test, expect} from "@playwright/test";
+import {test, expect } from "@playwright/test";
 
 test.describe("Preferences page", ()=>{
-    test("should have correct metadata and elements", async({ page })=>{
+    test("should have correct metadata and elements", async({ page, request })=>{
         await page.goto("/preferences");
         await expect(page).toHaveTitle("EventPop");
         await expect(page.getByRole("navigation")).toBeVisible();
@@ -21,8 +21,11 @@ test.describe("Preferences page", ()=>{
         await expect(page.getByLabel(preferenceLabels[i])).toBeVisible();
         }
         for(let i=0; i<preferenceLegends.length; i++){
-            await expect(page.getByRole("group", {name: preferenceLegends[i]})).toBeVisible();
-            }
+            const group = page.getByRole("group", {name: preferenceLegends[i]});
+            await expect(group).toBeVisible();
+            await expect(group.getByRole("checkbox")).not.toHaveCount(0);
+        }
+        // check that all options are populating for genres and event types
     }) 
 
     // test("should allow user to select new preferences and save them")
